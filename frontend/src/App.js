169d3a7e6a0febeef2175ff4d06a82d2fb27a6c9
Ashish Kumar <zashu001@gmail.com>
2023-04-30 23:06:43 +0530

    Initialized frontend and backend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="App">Hello World</div>
+      <div className="App">Line 2</div>
+    </>
+  );
+}
+
+export default App;
